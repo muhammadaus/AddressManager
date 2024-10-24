eefff0625b3b21760882eb772aaf1cda7b00ee5e
MUHAMMAD AUS <aushijree@gmail.com>
2024-10-24 21:48:24 +0900

    flattened and verified contracts

---
+++ flatten.sol
@@ -0,0 +1,8 @@
+   // SPDX-License-Identifier: MIT
+   pragma solidity ^0.8.0;
+
+   import "forge-flatten/Flatten.sol"; // Import the flattening library
+
+   contract Flattened is Flatten {
+       // This contract is just a placeholder to allow flattening
+   }
